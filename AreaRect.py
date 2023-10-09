a7f1ab50ebf03251c517d5687e3a2762913a1fd2
WhO-D-ShAziD <hshazidx@gmail.com>
2023-10-09 21:22:40 +0600

    Third Commit

---
+++ AreaRect.py
@@ -0,0 +1,10 @@
+# Taking the value of height from the user:
+height = float(input("Enter the value of height:\n"))
+
+# Taking the value of width from the user:
+width = float(input("Enter the value of width:\n"))
+
+# Calculating Area:
+area = height * width
+
+print("The area of the rectangle is: ", area)
