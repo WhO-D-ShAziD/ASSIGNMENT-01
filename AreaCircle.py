5b7893d91cc853c29f4861075c3d36a2e933222e
WhO-D-ShAziD <hshazidx@gmail.com>
2023-10-09 20:51:57 +0600

    first commit

---
+++ AreaCircle.py
@@ -0,0 +1,10 @@
+"""
+echo "# ASSIGNMENT-01" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/WhO-D-ShAziD/ASSIGNMENT-01.git
+git push -u origin main
+
+"""
