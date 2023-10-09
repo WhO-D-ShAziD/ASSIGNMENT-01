6596bffcb7b1ebbd1106400a8841bf3173d66756
WhO-D-ShAziD <hshazidx@gmail.com>
2023-10-09 21:07:27 +0600

    Second Commit

--- AreaCircle.py
+++ AreaCircle.py
@@ -8,3 +8,20 @@ git remote add origin https://github.com/WhO-D-ShAziD/ASSIGNMENT-01.git
 git push -u origin main
 
 """
+
+# Taking the radius value from the user:
+print("/////////// Area of Circle Calculation /////////////////\n")
+
+print("\n")
+
+r = float(input("Please enter the value of radius: \n"))
+
+
+
+# Declearing the value of Pi:
+pi = 3.1416
+
+# Area Calcualtion:
+area = pi * r * r
+
+print("The area of the circle is: \n", area)
