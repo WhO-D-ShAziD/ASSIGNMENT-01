9117ddf85641fcbe05475925d2cd9571d0961e0b
WhO-D-ShAziD <hshazidx@gmail.com>
2023-10-09 21:32:24 +0600

    Fourth Commit

--- AreaCircle.py
+++ AreaCircle.py
@@ -1,14 +1,3 @@
-"""
-echo "# ASSIGNMENT-01" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/WhO-D-ShAziD/ASSIGNMENT-01.git
-git push -u origin main
-
-"""
-
 # Taking the radius value from the user:
 print("/////////// Area of Circle Calculation /////////////////\n")
 
---
+++ AreaRombus.py
@@ -0,0 +1,11 @@
+# Taking first daiagonal value from the user:
+d1 = float(input("Please enter the value of the first diagonal:\n"))
+
+# Taking the second diagonal value from the user:
+d2  = float(input("Please enter the value of the second diagonal:\n"))
+
+# Calculationg area:
+
+area = 0.5 * d1 * d2
+
+print("The area of the rhombus is: ", area)
